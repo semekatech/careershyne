fd4334b3cc506d8862c9dbe57cf826fd796ab844
semekatech <georgemuemah@gmail.com>
2025-09-03 21:28:04 +0300

    order-form

--- frontend/src/pages/HowItWorks.vue
+++ frontend/src/pages/HowItWorks.vue
@@ -7,7 +7,11 @@
     <FooterSection />
   </main>
 </template>
-
+<script setup>
+import TheWelcome from "@/components/TheWelcome.vue";
+import cvForm from "@/components/CvForm.vue";
+import FooterSection from "@/components/FooterSection.vue";
+</script>
 
 <style scoped>
 .prose {
