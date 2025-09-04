8d979061785c6d5ef73203acfcf3502471e1db00
semekatech <georgemuemah@gmail.com>
2025-09-04 21:31:25 +0300

    order-form

--- frontend/src/components/PricingPlans.vue
+++ frontend/src/components/PricingPlans.vue
@@ -17,7 +17,7 @@
           <img class="relative z-20 inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://randomuser.me/api/portraits/women/2.jpg" alt="Member">
           <img class="relative z-10 inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://randomuser.me/api/portraits/women/3.jpg" alt="Member">
           <img class="relative z-0 inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://randomuser.me/api/portraits/men/4.jpg" alt="Member">
-          <span class="!ml-2 font-bold italic text-blue-500">+500 professionals already joined</span>
+          <span class="!ml-2 font-bold italic text-orange-500">500+ professionals already learning and growing with us</span>
         </div>
 
         <!-- CTA -->
@@ -25,9 +25,9 @@
           <a
             href="https://whatsapp.com/channel/0029VbBbuYSDZ4LYzXurHm21" 
             target="_blank"
-            class="text-md relative inline-flex items-center gap-x-2 rounded-lg bg-blue-600 px-6 py-4 font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            class="text-md relative inline-flex items-center gap-x-2 rounded-lg bg-orange-600 px-6 py-4 font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           >
-            <span class="absolute -top-5 left-0 w-full text-left text-xs italic text-blue-600">Free & No Spam</span>
+            <span class="absolute -top-5 left-0 w-full text-left text-xs italic text-orange-500">Free & No Spam</span>
             Join WhatsApp Channel
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="-mr-0.5 h-5 w-5">
               <path fill-rule="evenodd" d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z" clip-rule="evenodd"/>
