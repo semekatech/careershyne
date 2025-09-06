df56a13c5bf5eb188f209b7fa36e9b9a67b22057
semekatech <georgemuemah@gmail.com>
2025-09-06 11:28:40 +0300

    frontend merge

--- backend/resources/views/emails/order.blade.php
+++ backend/resources/views/emails/order.blade.php
@@ -27,7 +27,7 @@
 
                         <td style="padding: 20px; line-height: 1.6; color: #333333;">
                             <h2 style="color: #1a1a1a; font-size: 24px; margin-bottom: 10px;">Hi,
-                                {{  $details['order']->full_name }}!</h2>
+                                {{  $details['order']->fullname }}!</h2>
                             <p style="margin-bottom: 15px;">{{ $details['message'] }}</p>
 
                             <p style="margin-bottom: 15px;">If you have any questions or need support, please don't
