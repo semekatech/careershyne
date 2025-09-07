bf06d06658a3299f5a44b728c6b357f0728802eb
semekatech <georgemuemah@gmail.com>
2025-09-07 19:14:59 +0300

    adding reverse proxy for visitors

--- frontend/src/pages/DashboardHome.vue
+++ frontend/src/pages/DashboardHome.vue
@@ -5,8 +5,6 @@
 
 
 <script setup>
-import DashboardCharts from "@/components/DashboardCharts.vue";
-import PromoterChart from "@/components/PromoterChart.vue";
 import DashboardStats from "@/components/DashboardStats.vue";
 import { useAuthStore } from "@/stores/auth";
 const auth = useAuthStore();
