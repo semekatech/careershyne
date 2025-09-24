c19a6ff66b7e3bee17c2ee3c16ed8549c6f9a2db
semekatech <georgemuemah@gmail.com>
2025-09-24 21:17:00 +0300

    ui updateds

--- frontend/src/pages/SetupPage.vue
+++ frontend/src/pages/SetupPage.vue
@@ -102,7 +102,6 @@ import { ref, onMounted } from "vue";
 import { useAuthStore } from "@/stores/auth";
 import ProfileService from "@/services/profileService";
 import OptionsService from "@/services/optionsService";
-import { useAuthStore } from "@/stores/auth";
 const auth = useAuthStore();
 const userName = ref(auth.user?.name || "User");
 const industry = ref("");
