3ff41bcd3a784b326d495482f88a7adda2061f57
semekatech <georgemuemah@gmail.com>
2025-10-10 11:15:03 +0300

    updated pricing plans

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 Route::get('/', fn() => response()->json(['status' => 'Backend is working']));
+Route::prefix('api')->group(function () {
+    Route::post('auth/login', [AuthController::class, 'login']);
+});
