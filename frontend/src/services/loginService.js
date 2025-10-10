d04b5951fb4824e61b8cfd204c3467fa584547c2
semekatech <georgemuemah@gmail.com>
2025-10-10 10:26:55 +0300

    updated pricing plans

--- frontend/src/services/loginService.js
+++ frontend/src/services/loginService.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-const API = 'https://careershyne.com/api/auth/';
+const API = 'https://careershyne.com:8002/api/auth/';
 export default {
   post: (data) => {
     return axios.post(API + 'login', data); 
